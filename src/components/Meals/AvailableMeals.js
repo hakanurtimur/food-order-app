66993a510a9a631afe6b26de2acf77f487069c0a
Hakan Urtimur <dythakanurtimur@gmail.com>
2023-03-27 21:19:42 +0300

    send dummy meals to firebase and fetch them

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,55 +1,73 @@
-import React from "react";
+import React, { useEffect, useCallback } from "react";
 import Card from "../UI/Card/Card";
-import classes from './AvailableMeals.module.css'
+import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
-
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
 
 const AvailableMeals = () => {
+  let fetchedMeals = [];
 
-    const mealsList = DUMMY_MEALS.map(meal => <MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price}></MealItem>
+  const fetchMeals = useCallback(async () => {
+    const response = await fetch(
+      "https://react-http-3a15e-default-rtdb.firebaseio.com/meals.json"
     );
 
-    return(
-        <section className={classes.meals}>
+    const data = await response.json();
 
-        <Card>
-        <ul>
-        {mealsList}
-        </ul>
-        </Card>
-        
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    fetchedMeals = data;
+  }, []);
 
-        </section>
-    )
+  useEffect(() => {
+    fetchMeals();
+  }, [fetchMeals]);
 
+  console.log(fetchedMeals);
 
-}
+  const mealsList = fetchedMeals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    ></MealItem>
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+        <button onClick={fetchMeals}>FETCH</button>
+      </Card>
+    </section>
+  );
+};
 
 export default AvailableMeals;
